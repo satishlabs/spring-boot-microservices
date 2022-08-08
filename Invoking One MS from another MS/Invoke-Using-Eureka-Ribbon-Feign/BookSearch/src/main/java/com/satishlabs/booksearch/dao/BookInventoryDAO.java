cc995a82f5fd050d19e023ec4af7bc9925c0f71b
satishlabs <spd61185@gmail.com>
2022-08-08 23:34:42 +0530

    Updated unused app

--- Invoking One MS from another MS/Invoke-Using-Eureka-Ribbon-Feign/BookSearch/src/main/java/com/satishlabs/booksearch/dao/BookInventoryDAO.java
+++
@@ -1,9 +0,0 @@
-package com.satishlabs.booksearch.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.satishlabs.booksearch.entity.BookInventory;
-
-public interface BookInventoryDAO extends JpaRepository<BookInventory, Integer>{
-
-}
--- Invoking One MS from another MS/Invoke-Using-Eureka-Ribbon/BookSearch/src/main/java/com/satishlabs/booksearch/dao/BookRatingDAO.java
+++
@@ -1,9 +0,0 @@
-package com.satishlabs.booksearch.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.satishlabs.booksearch.entity.BookRating;
-
-public interface BookRatingDAO extends JpaRepository<BookRating, Integer> {
-	//public BookRating getBookRatingByBookId(Integer bookId);
-}
